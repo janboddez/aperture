9652fb936fafbb7c5bde0db54c3bce5744d79de8
Jan Boddez <jan.boddez@gmail.com>
2020-04-23 16:21:38 +0200

    Nested conditions

--- aperture/app/Http/Controllers/HomeController.php
+++ aperture/app/Http/Controllers/HomeController.php
@@ -37,173 +37,174 @@ class HomeController extends Controller
 
     public function channel(Channel $channel)
     {
-        if (Gate::allows('edit-channel', $channel)) {
-            $sources = $channel->sources()
-                ->withCount('entries')
-                ->get();
-
-            return view('channel', [
-                'channel' => $channel,
-                'sources' => $sources->sortBy('domain', SORT_NATURAL|SORT_FLAG_CASE),
-            ]);
-        } else {
+        if (! Gate::allows('edit-channel', $channel)) {
             abort(401);
         }
+
+        $sources = $channel->sources()
+            ->withCount('entries')
+            ->get();
+
+        return view('channel', [
+            'channel' => $channel,
+            // Sort by name (case-insensitive)
+            'sources' => $sources->sortBy('domain', SORT_NATURAL | SORT_FLAG_CASE),
+        ]);
     }
 
     public function add_source(Channel $channel)
     {
-        if (Gate::allows('edit-channel', $channel)) {
-            // Create or load the source
-            $source = Source::where('url', Request::input('url'))->first();
-            if (! $source) {
-                $source = new Source();
-                $source->created_by = Auth::user()->id;
-                $source->url = Request::input('url');
-                $source->format = Request::input('format');
-                $source->token = str_random(32);
-                $source->is_new = true;
-                $source->save();
-            }
-
-            if (0 == $channel->sources()->where('source_id', $source->id)->count()) {
-                $channel->sources()->attach($source->id, ['created_at'=>date('Y-m-d H:i:s')]);
-            }
-
-            event(new SourceAdded($source, $channel));
-
-            return response()->json([
-                'result' => 'ok',
-            ]);
-        } else {
+        if (! Gate::allows('edit-channel', $channel)) {
             abort(401);
         }
+
+        // Create or load the source
+        $source = Source::where('url', Request::input('url'))->first();
+        if (! $source) {
+            $source = new Source();
+            $source->created_by = Auth::user()->id;
+            $source->url = Request::input('url');
+            $source->format = Request::input('format');
+            $source->token = str_random(32);
+            $source->is_new = true;
+            $source->save();
+        }
+
+        if (0 === $channel->sources()->where('source_id', $source->id)->count()) {
+            $channel->sources()->attach($source->id, ['created_at' => date('Y-m-d H:i:s')]);
+        }
+
+        event(new SourceAdded($source, $channel));
+
+        return response()->json([
+            'result' => 'ok',
+        ]);
     }
 
     public function save_channel(Channel $channel)
     {
-        if (Gate::allows('edit-channel', $channel)) {
-            $channel->name = Request::input('name');
-
-            $channel->include_only = Request::input('include_only') ?: '';
-            $channel->exclude_types = Request::input('exclude_types') ?: '';
-
-            $keywords = preg_split('/[ ,]+/', Request::input('include_keywords'));
-            $channel->include_keywords = implode(' ', $keywords);
-
-            $keywords = preg_split('/[ ,]+/', Request::input('exclude_keywords'));
-            $channel->exclude_keywords = implode(' ', $keywords);
-
-            $channel->read_tracking_mode = Request::input('read_tracking_mode') ?: 'counts';
-
-            $channel->hide_in_demo_mode = Request::input('hide_in_demo_mode') ? 1 : 0;
-            $channel->archived = Request::input('archived') ? 1 : 0;
-
-            $channel->default_destination = Request::input('default_destination') ?: '';
-
-            // Users with an unlimited retention policy can override per channel
-            if (0 == Auth::user()->retention_days) {
-                $channel->retention_days = Request::input('retention_days') ?: 0;
-            }
-
-            $channel->save();
-
-            return response()->json([
-                'result' => 'ok',
-            ]);
-        } else {
+        if (! Gate::allows('edit-channel', $channel)) {
             abort(401);
         }
+
+        $channel->name = Request::input('name');
+
+        $channel->include_only = Request::input('include_only') ?: '';
+        $channel->exclude_types = Request::input('exclude_types') ?: '';
+
+        $keywords = preg_split('/[ ,]+/', Request::input('include_keywords'));
+        $channel->include_keywords = implode(' ', $keywords);
+
+        $keywords = preg_split('/[ ,]+/', Request::input('exclude_keywords'));
+        $channel->exclude_keywords = implode(' ', $keywords);
+
+        $channel->read_tracking_mode = Request::input('read_tracking_mode') ?: 'counts';
+
+        $channel->hide_in_demo_mode = Request::input('hide_in_demo_mode') ? 1 : 0;
+        $channel->archived = Request::input('archived') ? 1 : 0;
+
+        $channel->default_destination = Request::input('default_destination') ?: '';
+
+        // Users with an unlimited retention policy can override per channel
+        if (0 === Auth::user()->retention_days) {
+            $channel->retention_days = Request::input('retention_days') ?: 0;
+        }
+
+        $channel->save();
+
+        return response()->json([
+            'result' => 'ok',
+        ]);
     }
 
     public function save_source(Channel $channel, Source $source)
     {
-        if (Gate::allows('edit-channel', $channel)) {
-            $subscription = DB::table('channel_source')
-                ->where('channel_id', $channel->id)
-                ->where('source_id', $source->id)
-                ->update([
-                    'name' => Request::input('name'),
-                ]);
-
-            return response()->json([
-                'result' => 'ok',
-            ]);
-        } else {
+        if (! Gate::allows('edit-channel', $channel)) {
             abort(401);
         }
+
+        $subscription = DB::table('channel_source')
+            ->where('channel_id', $channel->id)
+            ->where('source_id', $source->id)
+            ->update([
+                'name' => Request::input('name'),
+            ]);
+
+        return response()->json([
+            'result' => 'ok',
+        ]);
     }
 
     public function remove_source(Channel $channel)
     {
-        if (Gate::allows('edit-channel', $channel)) {
-            $source = Source::where('id', Request::input('source_id'))->first();
-
-            if ($source) {
-                $channel->remove_source($source, (bool) Request::input('remove_entries'));
-            }
-
-            return response()->json([
-                'result' => 'ok',
-            ]);
-        } else {
+        if (! Gate::allows('edit-channel', $channel)) {
             abort(401);
         }
+
+        $source = Source::where('id', Request::input('source_id'))->first();
+
+        if ($source) {
+            $channel->remove_source($source, (bool) Request::input('remove_entries'));
+        }
+
+        return response()->json([
+            'result' => 'ok',
+        ]);
     }
 
     public function delete_channel(Channel $channel)
     {
-        if (Gate::allows('edit-channel', $channel)) {
-            if (in_array($channel->uid, ['notifications'])) {
-                abort(400);
-            }
-
-            $channel->entries()->delete();
-            $channel->delete();
-
-            return response()->json([
-                'result' => 'ok',
-            ]);
-        } else {
+        if (! Gate::allows('edit-channel', $channel)) {
             abort(401);
         }
+
+        if (in_array($channel->uid, ['notifications'])) {
+            abort(400);
+        }
+
+        $channel->entries()->delete();
+        $channel->delete();
+
+        return response()->json([
+            'result' => 'ok',
+        ]);
     }
 
     public function set_channel_order()
     {
         if (! is_array(Request::input('channels'))) {
-            return response()->json(['result'=>'error']);
+            return response()->json(['result' => 'error']);
         }
 
         $sorted = Auth::user()->set_channel_order(Request::input('channels'));
         if ($sorted) {
-            return response()->json(['result'=>'ok']);
+            return response()->json(['result' => 'ok']);
         } else {
-            return response()->json(['result'=>'error']);
+            return response()->json(['result' => 'error']);
         }
     }
 
     public function add_apikey(Channel $channel)
     {
-        if (Gate::allows('edit-channel', $channel)) {
-            // Create a new source for this API key
-            $source = new Source();
-            $source->token = str_random(32);
-            $source->name = Request::input('name') ?: '';
-            $source->format = 'apikey';
-            $source->created_by = Auth::user()->id;
-            // always download images when creating posts via micropub
-            $source->download_images = true;
-            $source->save();
-
-            $channel->sources()->attach($source->id, ['created_at'=>date('Y-m-d H:i:s')]);
-
-            return response()->json([
-                'result' => 'ok',
-            ]);
-        } else {
+        if (! Gate::allows('edit-channel', $channel)) {
             abort(401);
         }
+
+        // Create a new source for this API key
+        $source = new Source();
+        $source->token = str_random(32);
+        $source->name = Request::input('name') ?: '';
+        $source->format = 'apikey';
+        $source->created_by = Auth::user()->id;
+        // always download images when creating posts via micropub
+        $source->download_images = true;
+        $source->save();
+
+        $channel->sources()->attach($source->id, ['created_at'=>date('Y-m-d H:i:s')]);
+
+        return response()->json([
+            'result' => 'ok',
+        ]);
     }
 
     public function find_feeds()
